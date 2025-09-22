c72cd5edf08f7991ab5429fe98def683680b967e
anjaduras <anna@duras.org>
2025-09-22 13:54:50 +0200

    learning output and literals

---
+++ LiteraleBeispiel.java
@@ -0,0 +1,17 @@
+public class LiteraleBeispiel {
+    public static void main(String[] args) {
+        char zeichen = 'A';
+        int ganzzahl = 100;
+        double kommazahl = 1.344;
+        String text = "Sonne";
+        int ganzzahl2 = 100200300;
+        int ganzzahl3 = -12;
+        // Ausgabe
+        System.out.println("Zeichen: " + zeichen);
+        System.out.println("Ganzzahl: " + ganzzahl);
+        System.out.println("kommazahl: " + kommazahl);
+        System.out.println("Text: " + text);
+        System.out.println("Ganzzahl 2: " + ganzzahl);
+        System.out.println("Ganzzahl 3: " + ganzzahl);
+    }
+}
