9ce44bce92298cad91044733a7d02c32d997de15
anjaduras <anna@duras.org>
2025-10-06 14:36:17 +0200

    variables

---
+++ MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+//simple one just to memorise variables
+	public static void main(String[] args) {   
+String name = "Duras";
+int age = 20;
+double desiredSalary = 70.000;
+char gender = 'f';
+boolean lookingForJob = false;
+System.out.println(gender);
+	}
+}
